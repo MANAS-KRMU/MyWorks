92c93e4938979c79c6d1255c83411e5f849d3948
MANAS-KRMU <manaskumar32109@gmail.com>
2026-04-11 13:27:30 +0530

    practice

---
+++ Module_css_PRACTICE/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './Components/button/Button.jsx'
+import Header from './Components/header/Header.jsx'
+
+
+const App = () => {
+
+  return (
+    <div>
+      <Header />
+      <Button />
+    </div>
+  )
+}
+
+export default App
